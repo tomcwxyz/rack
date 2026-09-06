90eee252cde53eb2f7111591ab90d27e7f86878c
Tom C Watson <150357824+tomcwxyz@users.noreply.github.com>
2026-09-06 09:07:40 +0100

    feat(starter): expose combined Starter library

--- packages/starter/src/index.ts
+++ packages/starter/src/index.ts
@@ -1,1 +1,12 @@
-export * from "./catalogue.js";
+export * from "./library.js";
+export {
+  HONEY_SOURCE,
+  HONEY_UPSTREAM_REVISION,
+  honeyStarterCatalogue,
+  honeyStarterTemplates,
+} from "./honey.js";
+export type {
+  ExternalStarterEntry,
+  ExternalStarterSource,
+  StarterSourceProvenance,
+} from "./external.js";
