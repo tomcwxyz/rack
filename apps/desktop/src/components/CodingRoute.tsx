0565505d0b5715b10264f825edc08ce8be3a64e2
Tom C Watson <150357824+tomcwxyz@users.noreply.github.com>
2026-09-21 19:57:29 +0100

    feat(desktop): show starter pack in coding final review

--- apps/desktop/src/components/CodingRoute.tsx
+++ apps/desktop/src/components/CodingRoute.tsx
@@ -20,6 +20,7 @@ import {
 } from "./PracticeProposition.js";
 import { MaterialImport } from "./MaterialImport.js";
 import { SelectedStarterPack } from "./SelectedStarterPack.js";
+import { StarterPackReview } from "./StarterPackReview.js";
 import { TopoCreationContext } from "./TopoCreationContext.js";
 import "../proposition-creation.css";
 
@@ -395,6 +396,8 @@ export function CodingRoute({
               <p>{draft.taskPurpose}</p>
             </article>
           </div>
+
+          <StarterPackReview templateId={starterPackId} />
 
           <details className="file-plan">
             <summary>Show the files Rack will create</summary>
